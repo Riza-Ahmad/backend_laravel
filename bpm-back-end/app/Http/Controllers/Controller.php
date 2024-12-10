49410e552f652f57cbf67bbde8d9eeccb6dae69e
Riza-Ahmad <Riza-Ahmad>
2024-12-10 19:16:49 +0700

    done

--- bpm-back-end/app/Http/Controllers/Controller.php
+++ bpm-back-end/app/Http/Controllers/Controller.php
@@ -2,7 +2,39 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Dokumentasi API",
+ *      description="API Documentation",
+ *      @OA\Contact(
+ *          email="ekoabdulgoffar129@gmail.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ * 
+ * @OA\SecurityScheme(
+ *          securityScheme="bearerAuth",
+ *          type="http",
+ *          scheme="bearer",
+ *          description="Enter token"
+ * )
+ * 
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="API Documentation"
+ * )
+ */
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
